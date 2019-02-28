653ef4f3f4996cd490352f61117e3b1335d670f7
jamalamar <jamal_ab22@hotmail.com>
2019-02-28 10:09:02 -0600

    added imported array of 270,000+ words and css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Autocomplete from './autocomplete.js';
-
+import words from 'an-array-of-english-words'
 
 const App = () => {
-
   return (
     <div className="App">
+    <h1>Choose from 270,000 words!</h1>
       <Autocomplete
-        options={[
-          'Papaya',
-          'Persimmon',
-          'Paw Paw',
-          'Prickly Pear',
-          'Peach',
-          'Pomegranate',
-          'Pineapple'
-        ]}
+        options={words}
       />
     </div>
   );

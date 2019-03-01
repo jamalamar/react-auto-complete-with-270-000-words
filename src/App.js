e42dc4e942ad3844552fbf831168a6dad9d88885
jamalamar <jamal_ab22@hotmail.com>
2019-03-01 10:08:21 -0600

    created modal(not showing)

--- src/App.js
+++ src/App.js
@@ -2,16 +2,43 @@ import React, { Component } from 'react';
 import './App.css';
 import Autocomplete from './autocomplete.js';
 import words from 'an-array-of-english-words'
+import Modal from './modal.js'
 
-const App = () => {
+class App extends Component {
+
+	state = {
+    isModalOpen: false
+  	};
+
+  	toggleState = e => {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    });
+  	};
+
+  render(){
   return (
     <div className="App">
+      <div>
+        <button onClick={this.toggleState}>Open Modal</button>
+        <div>Modal is: {this.state.isModalOpen ? "Open" : "Closed"}</div>
+        {this.state.isModalOpen && (
+          <Modal
+            id="modal"
+            isOpen={this.state.isModalOpen}
+            onClose={this.toggleState}
+          >
+            <div className="box-body">I am the content of the modal</div>
+          </Modal>
+        )}
+      </div>
     <h1>Choose from 270,000 words!</h1>
       <Autocomplete
         options={words}
       />
     </div>
   );
+}
 };
 
 export default App;
